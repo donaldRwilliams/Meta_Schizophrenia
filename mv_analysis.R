c6cd0304eec4656c1a340621054e9bb4c95b85ba
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-06-05 23:06:46 +0200

    try out a prior on the regression parameters

--- mv_analysis.R
+++ mv_analysis.R
@@ -1,28 +1,35 @@
 library(brms)
-prior <- c(set_prior("normal(0,0.25)", class = "sd"))
+prior <- c(set_prior("normal(0,0.25)", class = "sd"),
+           set_prior("normal(0,0.5)"))
 conditions <- data.frame(study = unique(sdata$study))
 rownames(conditions) <- unique(sdata$study)
 
 fit_SMD_post <- brm(SMD_post ~ 0 + sympType + (0+sympType|study), 
                     data = sdata, autocor = cor_fixed(V_SMD_post), 
-                    prior = prior)
+                    prior = prior, sample_prior = TRUE)
 fit_SMD_post
 plot(fit_SMD_post, ask = FALSE)
+(hyp_SMD_post <- hypothesis(fit_SMD_post, paste("sympType", c("pos", "neg", "gen"), " = 0")))
+plot(hyp_SMD_post)
 marginal_effects(fit_SMD_post)
 plot(marginal_effects(fit_SMD_post, conditions = conditions, 
                       re_formula = NULL), points = TRUE, ncol = 4) + theme_bw()
 
 fit_SMCR <- brm(SMCR ~ 0 + sympType + (0+sympType|study), data = sdata,
-                autocor = cor_fixed(V_SMCR), prior = prior)
+                autocor = cor_fixed(V_SMCR), prior = prior,
+                sample_prior = TRUE)
 fit_SMCR
 plot(fit_SMCR, ask = FALSE)
+(hyp_SMCR <- hypothesis(fit_SMCR, paste("sympType", c("pos", "neg", "gen"), " = 0")))
+plot(hyp_SMCR)
 marginal_effects(fit_SMCR)
 plot(marginal_effects(fit_SMCR, conditions = conditions, 
                       re_formula = NULL), points = TRUE, ncol = 4)
 
 # test SMCR model without random effects
 fit_SMCR2 <- brm(SMCR ~ 0 + sympType, data = sdata,
-                 autocor = cor_fixed(V_SMCR), prior = prior)
+                 autocor = cor_fixed(V_SMCR), prior = prior,
+                 sample_prior = TRUE)
 fit_SMCR2
 plot(fit_SMCR2, ask = FALSE)
 marginal_effects(fit_SMCR2)

58de07a75964afc7522463042ebdc7683cb3d9dd
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-06-09 17:27:30 +0200

    update symptoms data preparation and analysis

---
+++ symptoms_analysis.R
@@ -0,0 +1,195 @@
+library(brms)
+prior <- c(set_prior("normal(0,0.3)", class = "sd"),
+           set_prior("normal(0,0.5)"))
+symp_type <- unique(sdata$sympType)
+conditions <- data.frame(study = unique(sdata$study))
+rownames(conditions) <- unique(sdata$study)
+theme_set(theme_bw())
+
+
+# ---------------- multivariate analysis ----------------
+# hedges' g estimates
+fit_SMD_post <- brm(SMD_post ~ 0 + sympType + (0+sympType|study), 
+                    data = sdata, autocor = cor_fixed(V_SMD_post), 
+                    prior = prior, sample_prior = TRUE)
+fit_SMD_post
+plot(fit_SMD_post, ask = FALSE)
+(hyp_SMD_post <- hypothesis(fit_SMD_post, paste("sympType", symp_type, " = 0")))
+plot(hyp_SMD_post)
+marginal_effects(fit_SMD_post)
+plot(marginal_effects(fit_SMD_post, conditions = conditions, 
+                      re_formula = NULL), points = TRUE, ncol = 4)
+
+# SMCR estimates
+fit_SMCR <- brm(SMCR ~ 0 + sympType + (0+sympType|study), data = sdata,
+                autocor = cor_fixed(V_SMCR), prior = prior,
+                sample_prior = TRUE)
+fit_SMCR
+plot(fit_SMCR, ask = FALSE)
+(hyp_SMCR <- hypothesis(fit_SMCR, paste("sympType", symp_type, " = 0")))
+plot(hyp_SMCR)
+marginal_effects(fit_SMCR)
+plot(marginal_effects(fit_SMCR, conditions = conditions, 
+                      re_formula = NULL), points = TRUE, ncol = 4)
+
+
+# ---------------- analysis of overall symptoms ---------------- 
+fit_SMD_post_ove <- brm(SMD_post | se(sqrt(vSMD_post)) ~ 0 + intercept + (1|study), 
+                        data = tdata, prior = prior, sample_prior = TRUE)
+fit_SMD_post_ove
+(hyp_SMD_post_ove <- hypothesis(fit_SMD_post_ove, "intercept = 0"))
+plot(hyp_SMD_post_ove)
+
+fit_SMCR_ove <- brm(SMCR | se(sqrt(vSMCR)) ~ 0 + intercept  + (1|study), 
+                    data = tdata, prior = prior, sample_prior = TRUE)
+fit_SMCR_ove
+(hyp_SMCR_ove <- hypothesis(fit_SMCR_ove, "intercept = 0"))
+plot(hyp_SMCR_ove)
+
+# ---------------- moderator analyses ---------------- 
+# assuming the same effects across symptom types
+# hedges' g estimates
+fit_SMD_oxyAge <- brm(SMD_post ~ 0 + sympType + oxyAge + (0+sympType|study), 
+                    data = sdata, autocor = cor_fixed(V_SMD_post), 
+                    prior = prior, sample_prior = TRUE)
+fit_SMD_oxyAge
+
+fit_SMD_plaAge <- update(fit_SMD_oxyAge, formula. = ~ . + plaAge - oxyAge,
+                         newdata = sdata)
+fit_SMD_plaAge
+
+fit_SMD_oxyMale <- update(fit_SMD_oxyAge, formula. = ~ . + oxyMale - oxyAge,
+                         newdata = sdata)
+fit_SMD_oxyMale
+
+fit_SMD_plaMale <- update(fit_SMD_oxyAge, formula. = ~ . + plaMale - oxyAge,
+                          newdata = sdata)
+fit_SMD_plaMale
+
+fit_SMD_training <- update(fit_SMD_oxyAge, formula. = ~ . + training - oxyAge,
+                          newdata = sdata)
+fit_SMD_training
+
+fit_SMD_duration <- update(fit_SMD_oxyAge, formula. = ~ . + duration - oxyAge,
+                           newdata = sdata)
+fit_SMD_duration
+
+fit_SMD_dailyDose <- update(fit_SMD_oxyAge, formula. = ~ . + dailyDose - oxyAge,
+                           newdata = sdata)
+fit_SMD_dailyDose
+
+fit_SMD_eachDose <- update(fit_SMD_oxyAge, formula. = ~ . + eachDose - oxyAge,
+                           newdata = sdata)
+fit_SMD_eachDose
+
+fit_SMD_admin_int <- update(fit_SMD_oxyAge, formula. = ~ . + admin_int - oxyAge,
+                           newdata = sdata)
+fit_SMD_admin_int
+
+# SMCR estimates
+fit_SMCR_oxyAge <- brm(SMCR ~ 0 + sympType + oxyAge + (0+sympType|study), 
+                      data = sdata, autocor = cor_fixed(V_SMCR), 
+                      prior = prior, sample_prior = TRUE)
+fit_SMCR_oxyAge
+
+fit_SMCR_plaAge <- update(fit_SMCR_oxyAge, formula. = ~ . + plaAge - oxyAge,
+                         newdata = sdata)
+fit_SMCR_plaAge
+
+fit_SMCR_oxyMale <- update(fit_SMCR_oxyAge, formula. = ~ . + oxyMale - oxyAge,
+                          newdata = sdata)
+fit_SMCR_oxyMale
+
+fit_SMCR_plaMale <- update(fit_SMCR_oxyAge, formula. = ~ . + plaMale - oxyAge,
+                          newdata = sdata)
+fit_SMCR_plaMale
+
+fit_SMCR_training <- update(fit_SMCR_oxyAge, formula. = ~ . + training - oxyAge,
+                           newdata = sdata)
+fit_SMCR_training
+
+fit_SMCR_duration <- update(fit_SMCR_oxyAge, formula. = ~ . + duration - oxyAge,
+                           newdata = sdata)
+fit_SMCR_duration
+
+fit_SMCR_dailyDose <- update(fit_SMCR_oxyAge, formula. = ~ . + dailyDose - oxyAge,
+                            newdata = sdata)
+fit_SMCR_dailyDose
+
+fit_SMCR_eachDose <- update(fit_SMCR_oxyAge, formula. = ~ . + eachDose - oxyAge,
+                           newdata = sdata)
+fit_SMCR_eachDose
+
+fit_SMCR_admin_int <- update(fit_SMCR_oxyAge, formula. = ~ . + admin_int - oxyAge,
+                            newdata = sdata)
+fit_SMCR_admin_int
+
+
+# ---------------- leave one out analysis ----------------
+study_names <- levels(sdata$study)
+fits_SMD_post <- fits_SMCR <-
+  setNames(vector("list", length(study_names)), study_names)
+for (i in seq_along(study_names)) {
+  print(study_names[i])
+  subdata <- droplevels(subset(sdata, study != study_names[i]))
+  sub_V_SMD_post <- cov_matrix(study_id = subdata$study, 
+                               out_id = subdata$sympType,
+                               v = subdata$vSMD_post, R = cor_mat)
+  sub_V_SMCR <- cov_matrix(study_id = subdata$study, 
+                           out_id = subdata$sympType,
+                           v = subdata$vSMCR, R = cor_mat)
+  fits_SMD_post[[i]] <- update(fit_SMD_post, newdata = subdata,
+                               autocor = cor_fixed(sub_V_SMD_post))
+  fits_SMCR[[i]] <- update(fit_SMCR, newdata = subdata,
+                           autocor = cor_fixed(sub_V_SMCR))
+}
+
+# ---------------- publication bias ----------------
+library(metafor)
+tiff("symptoms_funnel_plots.tif", height=1100, width=850)
+dcex <- 2
+par(mfrow=c(4, 2), mar = c(5, 5, 2, 2) + 0.1)
+# positive
+funnel(rma_SMD_pos <- rma(SMD_post ~ 1, vi = vSMD_post,
+                          data = subset(sdata, sympType == "positive")), 
+       xlab = "Positive symptoms: Hedges' g", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMD_pos, estimator = "L0")
+funnel(rma_SMCR_pos <- rma(SMCR ~ 1, vi = vSMCR,
+                           data = subset(sdata, sympType == "positive")), 
+       xlab = "Positive symptoms: SCMR", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMCR_pos, estimator = "L0")
+# negative
+funnel(rma_SMD_neg <- rma(SMD_post ~ 1, vi = vSMD_post,
+                          data = subset(sdata, sympType == "negative")), 
+       xlab = "Negative symptoms: Hedges' g", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMD_neg, estimator = "L0")
+funnel(rma_SMCR_neg <- rma(SMCR ~ 1, vi = vSMCR,
+                           data = subset(sdata, sympType == "negative")), 
+       xlab = "Negative symptoms: SCMR", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMCR_neg, estimator = "L0")
+# general
+funnel(rma_SMD_gen <- rma(SMD_post ~ 1, vi = vSMD_post,
+                          data = subset(sdata, sympType == "general")), 
+       xlab = "General symptoms: Hedges' g", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMD_gen, estimator = "L0")
+funnel(rma_SMCR_gen <- rma(SMCR ~ 1, vi = vSMCR,
+                           data = subset(sdata, sympType == "general")), 
+       xlab = "General symptoms: SCMR", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMCR_gen, estimator = "L0")
+# overall
+funnel(rma_SMD_ove <- rma(SMD_post ~ 1, vi = vSMD_post, data = tdata), 
+       xlab = "Overall symptoms: Hedges' g", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMD_ove, estimator = "L0")
+funnel(rma_SMCR_ove <- rma(SMCR ~ 1, vi = vSMCR, data = tdata), 
+       xlab = "General symptoms: SCMR", cex = dcex, 
+       cex.axis = dcex, cex.lab = dcex)
+trimfill(rma_SMCR_ove, estimator = "L0")
+par(mfrow=c(1,1), mar = c(5, 4, 4, 2) + 0.1)
+dev.off()
---
+++ symptoms_data_preparation.R
@@ -0,0 +1,80 @@
+adata <- read.csv2("symptomsMod.csv")
+adata$obs <- factor(1:nrow(adata))
+
+library(metafor)
+# Hedges'g estimates
+adata <- escalc(measure="SMD", m1i=oxyMean_post, sd1i=oxySd_post, 
+                n1i=oxyN, m2i=plaMean_post, sd2i=plaSd_post, 
+                n2i=plaN, data=adata, var.names = c("SMD_post", "vSMD_post"))
+
+adata <- escalc(measure="SMD", m1i=oxyMean_pre, sd1i=oxySd_pre, 
+                n1i=oxyN, m2i=plaMean_pre, sd2i=plaSd_pre, 
+                n2i=plaN, data=adata, var.names = c("SMD_pre", "vSMD_pre"))
+
+# assumed pre-post correlation
+adata$ri <- 0.5
+
+# SMCR estimates
+# standardize based on pre-treatment SD
+adata <- escalc(measure="SMCR", m1i=oxyMean_post, m2i=oxyMean_pre,
+                sd1i=oxySd_pre, ni=oxyN, ri = ri,
+                data=adata, var.names = c("oxySMCR", "voxySMCR"))
+
+adata <- escalc(measure="SMCR", m1i=plaMean_post, m2i=plaMean_pre,
+                sd1i=plaSd_pre, ni=plaN, ri = ri,
+                data=adata, var.names = c("plaSMCR", "vplaSMCR"))
+
+adata$SMCR <- adata$oxySMCR - adata$plaSMCR
+adata$vSMCR <- adata$voxySMCR + adata$vplaSMCR
+
+# split up the data frame
+sdata <- droplevels(subset(adata, sympType != "overall"))
+tdata <- droplevels(subset(adata, sympType == "overall"))
+
+# compute covariance matrix of the effect sizes
+cov_matrix <- function(study_id, out_id, v, R, na.rm = FALSE) {
+  # Args:
+  #   study_id: a vector of study / sample IDs
+  #   out_id: a factor defining outcome IDS
+  #   v: a vector containing the variances
+  #   R: a symmetric matrix containing correlation
+  #        between different levels of x
+  out_id <- as.character(out_id)
+  stopifnot(isSymmetric(unname(R)), 
+            nrow(R) == length(unique(out_id)),
+            all(rownames(R) %in% unique(out_id)), 
+            length(study_id) == length(out_id),
+            length(study_id) == length(v))
+  colnames(R) <- rownames(R)
+  mat <- diag(v)
+  se <- sqrt(v)
+  se[is.na(se)] <- 0
+  if (length(study_id) > 1L) {
+    for (i in 2:nrow(mat)) {
+      for (j in 1:(i-1)) {
+        if (study_id[i] == study_id[j]) {
+          mat[i, j] <- mat[j, i] <- R[out_id[i], out_id[j]] * se[i] * se[j] 
+        }
+      }
+    }
+  }
+  dimnames(mat) <- list(1:nrow(mat), 1:ncol(mat))
+  if (na.rm) {
+    keep <- !is.na(diag(mat))
+    mat <- mat[keep, keep]
+  }
+  mat
+}
+
+# correlations based on Peralta and Cuesta 1993
+cor_mat <- matrix(c(1, 0.09, 0.20, 0.09, 1, 0.55, 0.20, 0.55, 1), 3, 3)
+rownames(cor_mat) <- levels(sdata$sympType)
+V_SMD_pre <- cov_matrix(study_id = sdata$study, out_id = sdata$sympType,
+                        v = sdata$vSMD_pre, R = cor_mat)
+
+V_SMD_post <- cov_matrix(study_id = sdata$study, out_id = sdata$sympType,
+                         v = sdata$vSMD_post, R = cor_mat)
+
+V_SMCR <- cov_matrix(study_id = sdata$study, out_id = sdata$sympType,
+                     v = sdata$vSMCR, R = cor_mat)
+

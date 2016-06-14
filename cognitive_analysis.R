1f80a22f0e40ad73e5dfbaa1213fb6c7a0122512
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-06-14 17:14:43 +0200

    analyse cognitive subgroups

--- cognitive_analysis.R
+++ cognitive_analysis.R
@@ -22,3 +22,66 @@ fit_SMCR
 plot(fit_SMCR, ask = FALSE)
 (hyp_SMCR <- hypothesis(fit_SMCR, "intercept = 0"))
 plot(hyp_SMCR)
+
+
+# ------- moderator analysis ------
+# start with subgroups
+subgroups1 <- levels(cdata$subgroup_1)
+subgroups2 <- levels(cdata$subgroup_2)
+subgroups3 <- levels(cdata$subgroup_3)
+
+# Hedges' g
+fit_SMD_sg1 <- brm(SMD_post ~ 0 + subgroup_1 + (1|obs), 
+                   data = cdata, autocor = cor_fixed(V_SMD_post), 
+                   prior = prior, sample_prior = TRUE)
+fit_SMD_sg1
+plot(fit_SMD_sg1, ask = FALSE)
+(hyp_SMD_sg1 <- hypothesis(fit_SMD_sg1, paste("subgroup_1", subgroups1, " = 0")))
+plot(hyp_SMD_sg1, chars = NULL, ask = FALSE)
+marginal_effects(fit_SMD_sg1)
+
+fit_SMD_sg2 <- brm(SMD_post ~ 0 + subgroup_2 + (1|obs), 
+                   data = cdata, autocor = cor_fixed(V_SMD_post), 
+                   prior = prior, sample_prior = TRUE)
+fit_SMD_sg2
+plot(fit_SMD_sg2, ask = FALSE)
+(hyp_SMD_sg2 <- hypothesis(fit_SMD_sg2, paste("subgroup_2", subgroups2, " = 0")))
+plot(hyp_SMD_sg2, chars = NULL, ask = FALSE, N = 6)
+marginal_effects(fit_SMD_sg2)
+
+fit_SMD_sg3 <- brm(SMD_post ~ 0 + subgroup_3 + (1|obs), 
+                        data = cdata, autocor = cor_fixed(V_SMD_post), 
+                        prior = prior, sample_prior = TRUE)
+fit_SMD_sg3
+plot(fit_SMD_sg3, ask = FALSE)
+(hyp_SMD_sg3 <- hypothesis(fit_SMD_sg3, paste("subgroup_3", subgroups3, " = 0")))
+plot(hyp_SMD_sg3, chars = NULL, ask = FALSE, N = 6)
+marginal_effects(fit_SMD_sg3)
+
+# SMCR
+fit_SMCR_sg1 <- brm(SMCR ~ 0 + subgroup_1 + (1|obs), 
+                        data = cdata, autocor = cor_fixed(V_SMCR), 
+                        prior = prior, sample_prior = TRUE)
+fit_SMCR_sg1
+plot(fit_SMCR_sg1, ask = FALSE)
+(hyp_SMCR_sg1 <- hypothesis(fit_SMCR_sg1, paste("subgroup_1", subgroups1, " = 0")))
+plot(hyp_SMCR_sg1, chars = NULL, ask = FALSE)
+marginal_effects(fit_SMCR_sg1)
+
+fit_SMCR_sg2 <- brm(SMCR ~ 0 + subgroup_2 + (1|obs), 
+                    data = cdata, autocor = cor_fixed(V_SMCR), 
+                    prior = prior, sample_prior = TRUE)
+fit_SMCR_sg2
+plot(fit_SMCR_sg2, ask = FALSE)
+(hyp_SMCR_sg2 <- hypothesis(fit_SMCR_sg2, paste("subgroup_2", subgroups2, " = 0")))
+plot(hyp_SMCR_sg2, chars = NULL, ask = FALSE, N = 6)
+marginal_effects(fit_SMCR_sg2)
+
+fit_SMCR_sg3 <- brm(SMCR ~ 0 + subgroup_3 + (1|obs), 
+                    data = cdata, autocor = cor_fixed(V_SMCR), 
+                    prior = prior, sample_prior = TRUE)
+fit_SMCR_sg3
+plot(fit_SMCR_sg3, ask = FALSE)
+(hyp_SMCR_sg3 <- hypothesis(fit_SMCR_sg3, paste("subgroup_3", subgroups3, " = 0")))
+plot(hyp_SMCR_sg3, chars = NULL, ask = FALSE, N = 6)
+marginal_effects(fit_SMCR_sg3)

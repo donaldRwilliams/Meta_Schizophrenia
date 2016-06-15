27d02ac57868aabbfceb3287e4f05cbbaaae555c
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-06-15 16:18:55 +0200

    cognitive analysis: use 2 cores for model fitting

--- cognitive_analysis.R
+++ cognitive_analysis.R
@@ -33,7 +33,7 @@ subgroups3 <- levels(cdata$subgroup_3)
 ### subgroups
 fit_SMD_sg1 <- brm(SMD_post ~ 0 + subgroup_1 + (1|obs), 
                    data = cdata, autocor = cor_fixed(V_SMD_post), 
-                   prior = prior, sample_prior = TRUE)
+                   prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMD_sg1
 plot(fit_SMD_sg1, ask = FALSE)
 (hyp_SMD_sg1 <- hypothesis(fit_SMD_sg1, paste("subgroup_1", subgroups1, " = 0")))
@@ -42,7 +42,7 @@ marginal_effects(fit_SMD_sg1)
 
 fit_SMD_sg2 <- brm(SMD_post ~ 0 + subgroup_2 + (1|obs), 
                    data = cdata, autocor = cor_fixed(V_SMD_post), 
-                   prior = prior, sample_prior = TRUE)
+                   prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMD_sg2
 plot(fit_SMD_sg2, ask = FALSE)
 (hyp_SMD_sg2 <- hypothesis(fit_SMD_sg2, paste("subgroup_2", subgroups2, " = 0")))
@@ -51,7 +51,7 @@ marginal_effects(fit_SMD_sg2)
 
 fit_SMD_sg3 <- brm(SMD_post ~ 0 + subgroup_3 + (1|obs), 
                         data = cdata, autocor = cor_fixed(V_SMD_post), 
-                        prior = prior, sample_prior = TRUE)
+                        prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMD_sg3
 plot(fit_SMD_sg3, ask = FALSE)
 (hyp_SMD_sg3 <- hypothesis(fit_SMD_sg3, paste("subgroup_3", subgroups3, " = 0")))
@@ -61,46 +61,46 @@ marginal_effects(fit_SMD_sg3)
 ### other moderators
 fit_SMD_oxyAge <- brm(SMD_post ~ oxyAge + (1|obs), 
                       data = cdata, autocor = cor_fixed(V_SMD_post), 
-                      prior = prior, sample_prior = TRUE)
+                      prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMD_oxyAge
 
 fit_SMD_plaAge <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + plaAge,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_plaAge
 
 fit_SMD_oxyMale <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + oxyMale,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_oxyMale
 
 fit_SMD_plaMale <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + plaMale,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_plaMale
 
 fit_SMD_training <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + training,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_training
 
 fit_SMD_duration <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + duration,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_duration
 
 fit_SMD_dailyDose <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + dailyDose,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_dailyDose
 
 fit_SMD_eachDose <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + eachDose,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_eachDose
 
 fit_SMD_admin_int <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + admin_int,
-                         newdata = cdata)
+                         newdata = cdata, cores = 2)
 fit_SMD_admin_int
 
 ## SMCR
 ### subgroups
 fit_SMCR_sg1 <- brm(SMCR ~ 0 + subgroup_1 + (1|obs), 
                         data = cdata, autocor = cor_fixed(V_SMCR), 
-                        prior = prior, sample_prior = TRUE)
+                        prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMCR_sg1
 plot(fit_SMCR_sg1, ask = FALSE)
 (hyp_SMCR_sg1 <- hypothesis(fit_SMCR_sg1, paste("subgroup_1", subgroups1, " = 0")))
@@ -109,7 +109,7 @@ marginal_effects(fit_SMCR_sg1)
 
 fit_SMCR_sg2 <- brm(SMCR ~ 0 + subgroup_2 + (1|obs), 
                     data = cdata, autocor = cor_fixed(V_SMCR), 
-                    prior = prior, sample_prior = TRUE)
+                    prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMCR_sg2
 plot(fit_SMCR_sg2, ask = FALSE)
 (hyp_SMCR_sg2 <- hypothesis(fit_SMCR_sg2, paste("subgroup_2", subgroups2, " = 0")))
@@ -118,7 +118,7 @@ marginal_effects(fit_SMCR_sg2)
 
 fit_SMCR_sg3 <- brm(SMCR ~ 0 + subgroup_3 + (1|obs), 
                     data = cdata, autocor = cor_fixed(V_SMCR), 
-                    prior = prior, sample_prior = TRUE)
+                    prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMCR_sg3
 plot(fit_SMCR_sg3, ask = FALSE)
 (hyp_SMCR_sg3 <- hypothesis(fit_SMCR_sg3, paste("subgroup_3", subgroups3, " = 0")))
@@ -128,37 +128,37 @@ marginal_effects(fit_SMCR_sg3)
 ### other moderators
 fit_SMCR_oxyAge <- brm(SMCR ~ oxyAge + (1|obs), 
                       data = cdata, autocor = cor_fixed(V_SMCR), 
-                      prior = prior, sample_prior = TRUE)
+                      prior = prior, sample_prior = TRUE, cores = 2)
 fit_SMCR_oxyAge
 
 fit_SMCR_plaAge <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + plaAge,
-                         newdata = cdata)
+                          newdata = cdata, cores = 2)
 fit_SMCR_plaAge
 
 fit_SMCR_oxyMale <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + oxyMale,
-                          newdata = cdata)
+                           newdata = cdata, cores = 2)
 fit_SMCR_oxyMale
 
 fit_SMCR_plaMale <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + plaMale,
-                          newdata = cdata)
+                          newdata = cdata, cores = 2)
 fit_SMCR_plaMale
 
 fit_SMCR_training <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + training,
-                           newdata = cdata)
+                           newdata = cdata, cores = 2)
 fit_SMCR_training
 
 fit_SMCR_duration <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + duration,
-                           newdata = cdata)
+                           newdata = cdata, cores = 2)
 fit_SMCR_duration
 
 fit_SMCR_dailyDose <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + dailyDose,
-                            newdata = cdata)
+                            newdata = cdata, cores = 2)
 fit_SMCR_dailyDose
 
 fit_SMCR_eachDose <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + eachDose,
-                           newdata = cdata)
+                           newdata = cdata, cores = 2)
 fit_SMCR_eachDose
 
 fit_SMCR_admin_int <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + admin_int,
-                            newdata = cdata)
+                            newdata = cdata, cores = 2)
 fit_SMCR_admin_int

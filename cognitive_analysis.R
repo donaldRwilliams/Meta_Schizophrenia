3122b75451ef7365f2460265de739681a3e4eddb
Paul-Christian Bürkner <paul.buerkner@gmail.com>
2016-06-17 13:54:58 +0200

    increase the number of MCMC iterations and use 2 cores by default in all of the analysis

--- cognitive_analysis.R
+++ cognitive_analysis.R
@@ -1,13 +1,17 @@
 library(brms)
 prior <- c(set_prior("cauchy(0,0.3)", class = "sd"),
            set_prior("normal(0,0.5)"))
+iter <- 5000
+options(mc.cores = 2)
+control <- list(adapt_delta = 0.95)
 theme_set(theme_bw())
 
 # ---------- primary meta-analysis ----------------
 ## SMD
 fit_SMD_post <- brm(SMD_post ~ 0 + intercept + (1|obs), 
                     data = cdata, autocor = cor_fixed(V_SMD_post), 
-                    prior = prior, sample_prior = TRUE)
+                    prior = prior, sample_prior = TRUE,
+                    iter = iter, control = control)
 fit_SMD_post
 plot(fit_SMD_post, ask = FALSE)
 (hyp_SMD_post <- hypothesis(fit_SMD_post, "intercept = 0"))
@@ -16,7 +20,8 @@ plot(hyp_SMD_post)
 ## SMCR
 fit_SMCR <- brm(SMCR ~ 0 + intercept + (1|obs), 
                 data = cdata, autocor = cor_fixed(V_SMCR), 
-                prior = prior, sample_prior = TRUE)
+                prior = prior, sample_prior = TRUE,
+                iter = iter, control = control)
 fit_SMCR
 plot(fit_SMCR, ask = FALSE)
 (hyp_SMCR <- hypothesis(fit_SMCR, "intercept = 0"))
@@ -32,7 +37,8 @@ subgroups3 <- levels(cdata$subgroup_3)
 ### subgroups
 fit_SMD_sg1 <- brm(SMD_post ~ 0 + subgroup_1 + (1|obs), 
                    data = cdata, autocor = cor_fixed(V_SMD_post), 
-                   prior = prior, sample_prior = TRUE, cores = 2)
+                   prior = prior, sample_prior = TRUE,
+                   iter = iter, control = control)
 fit_SMD_sg1
 plot(fit_SMD_sg1, ask = FALSE)
 (hyp_SMD_sg1 <- hypothesis(fit_SMD_sg1, paste("subgroup_1", subgroups1, " = 0")))
@@ -41,7 +47,8 @@ marginal_effects(fit_SMD_sg1)
 
 fit_SMD_sg2 <- brm(SMD_post ~ 0 + subgroup_2 + (1|obs), 
                    data = cdata, autocor = cor_fixed(V_SMD_post), 
-                   prior = prior, sample_prior = TRUE, cores = 2)
+                   prior = prior, sample_prior = TRUE,
+                   iter = iter, control = control)
 fit_SMD_sg2
 plot(fit_SMD_sg2, ask = FALSE)
 (hyp_SMD_sg2 <- hypothesis(fit_SMD_sg2, paste("subgroup_2", subgroups2, " = 0")))
@@ -49,8 +56,9 @@ plot(hyp_SMD_sg2, chars = NULL, ask = FALSE, N = 6)
 marginal_effects(fit_SMD_sg2)
 
 fit_SMD_sg3 <- brm(SMD_post ~ 0 + subgroup_3 + (1|obs), 
-                        data = cdata, autocor = cor_fixed(V_SMD_post), 
-                        prior = prior, sample_prior = TRUE, cores = 2)
+                   data = cdata, autocor = cor_fixed(V_SMD_post), 
+                   prior = prior, sample_prior = TRUE,
+                   iter = iter, control = control)
 fit_SMD_sg3
 plot(fit_SMD_sg3, ask = FALSE)
 (hyp_SMD_sg3 <- hypothesis(fit_SMD_sg3, paste("subgroup_3", subgroups3, " = 0")))
@@ -60,46 +68,48 @@ marginal_effects(fit_SMD_sg3)
 ### other moderators
 fit_SMD_oxyAge <- brm(SMD_post ~ oxyAge + (1|obs), 
                       data = cdata, autocor = cor_fixed(V_SMD_post), 
-                      prior = prior, sample_prior = TRUE, cores = 2)
+                      prior = prior, sample_prior = TRUE,
+                      iter = iter, control = control)
 fit_SMD_oxyAge
 
 fit_SMD_plaAge <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + plaAge,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_plaAge
 
 fit_SMD_oxyMale <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + oxyMale,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_oxyMale
 
 fit_SMD_plaMale <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + plaMale,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_plaMale
 
 fit_SMD_training <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + training,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_training
 
 fit_SMD_duration <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + duration,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_duration
 
 fit_SMD_dailyDose <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + dailyDose,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_dailyDose
 
 fit_SMD_eachDose <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + eachDose,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_eachDose
 
 fit_SMD_admin_int <- update(fit_SMD_oxyAge, formula. = ~ . - oxyAge + admin_int,
-                         newdata = cdata, cores = 2)
+                         newdata = cdata, control = control)
 fit_SMD_admin_int
 
 ## SMCR
 ### subgroups
 fit_SMCR_sg1 <- brm(SMCR ~ 0 + subgroup_1 + (1|obs), 
-                        data = cdata, autocor = cor_fixed(V_SMCR), 
-                        prior = prior, sample_prior = TRUE, cores = 2)
+                    data = cdata, autocor = cor_fixed(V_SMCR), 
+                    prior = prior, sample_prior = TRUE,
+                    iter = iter, control = control)
 fit_SMCR_sg1
 plot(fit_SMCR_sg1, ask = FALSE)
 (hyp_SMCR_sg1 <- hypothesis(fit_SMCR_sg1, paste("subgroup_1", subgroups1, " = 0")))
@@ -108,7 +118,8 @@ marginal_effects(fit_SMCR_sg1)
 
 fit_SMCR_sg2 <- brm(SMCR ~ 0 + subgroup_2 + (1|obs), 
                     data = cdata, autocor = cor_fixed(V_SMCR), 
-                    prior = prior, sample_prior = TRUE, cores = 2)
+                    prior = prior, sample_prior = TRUE,
+                    iter = iter, control = control)
 fit_SMCR_sg2
 plot(fit_SMCR_sg2, ask = FALSE)
 (hyp_SMCR_sg2 <- hypothesis(fit_SMCR_sg2, paste("subgroup_2", subgroups2, " = 0")))
@@ -117,7 +128,8 @@ marginal_effects(fit_SMCR_sg2)
 
 fit_SMCR_sg3 <- brm(SMCR ~ 0 + subgroup_3 + (1|obs), 
                     data = cdata, autocor = cor_fixed(V_SMCR), 
-                    prior = prior, sample_prior = TRUE, cores = 2)
+                    prior = prior, sample_prior = TRUE,
+                    iter = iter, control = control)
 fit_SMCR_sg3
 plot(fit_SMCR_sg3, ask = FALSE)
 (hyp_SMCR_sg3 <- hypothesis(fit_SMCR_sg3, paste("subgroup_3", subgroups3, " = 0")))
@@ -126,40 +138,41 @@ marginal_effects(fit_SMCR_sg3)
 
 ### other moderators
 fit_SMCR_oxyAge <- brm(SMCR ~ oxyAge + (1|obs), 
-                      data = cdata, autocor = cor_fixed(V_SMCR), 
-                      prior = prior, sample_prior = TRUE, cores = 2)
+                       data = cdata, autocor = cor_fixed(V_SMCR), 
+                       prior = prior, sample_prior = TRUE,
+                       iter = iter, control = control)
 fit_SMCR_oxyAge
 
 fit_SMCR_plaAge <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + plaAge,
-                          newdata = cdata, cores = 2)
+                          newdata = cdata, control = control)
 fit_SMCR_plaAge
 
 fit_SMCR_oxyMale <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + oxyMale,
-                           newdata = cdata, cores = 2)
+                           newdata = cdata, control = control)
 fit_SMCR_oxyMale
 
 fit_SMCR_plaMale <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + plaMale,
-                          newdata = cdata, cores = 2)
+                          newdata = cdata, control = control)
 fit_SMCR_plaMale
 
 fit_SMCR_training <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + training,
-                           newdata = cdata, cores = 2)
+                           newdata = cdata, control = control)
 fit_SMCR_training
 
 fit_SMCR_duration <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + duration,
-                           newdata = cdata, cores = 2)
+                           newdata = cdata, control = control)
 fit_SMCR_duration
 
 fit_SMCR_dailyDose <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + dailyDose,
-                            newdata = cdata, cores = 2)
+                            newdata = cdata, control = control)
 fit_SMCR_dailyDose
 
 fit_SMCR_eachDose <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + eachDose,
-                           newdata = cdata, cores = 2)
+                           newdata = cdata, control = control)
 fit_SMCR_eachDose
 
 fit_SMCR_admin_int <- update(fit_SMCR_oxyAge, formula. = ~ . - oxyAge + admin_int,
-                            newdata = cdata, cores = 2)
+                            newdata = cdata, control = control)
 fit_SMCR_admin_int
 
 
@@ -176,13 +189,33 @@ for (i in seq_along(study_names)) {
                             v = subdata$vSMCR, r = 0.7)
   fits_SMD_post[[i]] <- update(fit_SMD_post, newdata = subdata,
                                autocor = cor_fixed(sub_V_SMD_post),
-                               cores = 2)
+                               control = control)
   fits_SMCR[[i]] <- update(fit_SMCR, newdata = subdata,
                            autocor = cor_fixed(sub_V_SMCR), 
-                           cores = 2)
+                           control = control)
 }
 fits_SMD_post
 fits_SMCR
+
+
+# ---------- vary between-outcome correlation -------------
+rs <- c(0.1, 0.3, 0.5, 0.7, 0.9)
+fits_SMD_post_r <- fits_SMCR_r <-  
+  setNames(vector("list", length(rs)), rs)
+for (i in seq_along(rs)) {
+  V_SMD_post_r <- cov_matrix2(study_id = cdata$study, 
+                              v = cdata$vSMD_post, r = rs[i])
+  V_SMCR_r <- cov_matrix2(study_id = cdata$study,
+                          v = cdata$vSMCR, r = rs[i])
+  fits_SMD_post_r[[i]] <- update(fit_SMD_post, 
+                                 autocor = cor_fixed(V_SMD_post_r),
+                                 control = control)
+  fits_SMCR_r[[i]] <- update(fit_SMCR, autocor = cor_fixed(V_SMCR_r),
+                             control = control)
+}
+fits_SMD_post_r
+fits_SMCR_r
+
 
 # ------- publication bias ---------------
 library(metafor)
@@ -192,7 +225,7 @@ par(mfrow=c(4, 2), mar = c(5, 5, 2, 2) + 0.1)
 # general_cog
 funnel(rma_SMD_gcog <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(cdata, subgroup_1 == "general_cog")), 
-       xlab = "General cognitions: Hedges' g", cex = dcex, 
+       xlab = "General cognitions: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_gcog, estimator = "L0")
 funnel(rma_SMCR_gcog <- rma(SMCR ~ 1, vi = vSMCR,
@@ -203,7 +236,7 @@ trimfill(rma_SMCR_gcog, estimator = "L0")
 # social_cog
 funnel(rma_SMD_scog <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(cdata, subgroup_1 == "social_cog")), 
-       xlab = "Social cognitions: Hedges' g", cex = dcex, 
+       xlab = "Social cognitions: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_scog, estimator = "L0")
 funnel(rma_SMCR_scog <- rma(SMCR ~ 1, vi = vSMCR,
@@ -214,7 +247,7 @@ trimfill(rma_SMCR_scog, estimator = "L0")
 # social_func
 funnel(rma_SMD_sfunc <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(cdata, subgroup_1 == "social_func")), 
-       xlab = "Social functions: Hedges' g", cex = dcex, 
+       xlab = "Social functions: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_sfunc, estimator = "L0")
 funnel(rma_SMCR_sfunc <- rma(SMCR ~ 1, vi = vSMCR,
@@ -225,7 +258,7 @@ trimfill(rma_SMCR_sfunc, estimator = "L0")
 # symptoms
 funnel(rma_SMD_symp <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(cdata, subgroup_1 == "symptoms")), 
-       xlab = "Symptoms: Hedges' g", cex = dcex, 
+       xlab = "Symptoms: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_symp, estimator = "L0")
 funnel(rma_SMCR_symp <- rma(SMCR ~ 1, vi = vSMCR,
@@ -235,20 +268,3 @@ funnel(rma_SMCR_symp <- rma(SMCR ~ 1, vi = vSMCR,
 trimfill(rma_SMCR_symp, estimator = "L0")
 par(mfrow=c(1,1), mar = c(5, 4, 4, 2) + 0.1)
 dev.off()
-
-
-# ---------- vary between outcome correlation -------------
-rs <- c(0.1, 0.3, 0.5, 0.7, 0.9)
-fits_SMD_post_r <- fits_SMCR_r <-  
-  setNames(vector("list", length(rs)), rs)
-for (i in seq_along(rs)) {
-  V_SMD_post_r <- cov_matrix2(study_id = cdata$study, 
-                              v = cdata$vSMD_post, r = rs[i])
-  V_SMCR_r <- cov_matrix2(study_id = cdata$study,
-                          v = cdata$vSMCR, r = rs[i])
-  fits_SMD_post_r[[i]] <- update(fit_SMD_post, 
-                                 autocor = cor_fixed(V_SMD_post_r))
-  fits_SMCR_r[[i]] <- update(fit_SMCR, autocor = cor_fixed(V_SMCR_r))
-}
-fits_SMD_post_r
-fits_SMCR_r
--- symptoms_analysis.R
+++ symptoms_analysis.R
@@ -1,6 +1,9 @@
 library(brms)
 prior <- c(set_prior("cauchy(0,0.3)", class = "sd"),
            set_prior("normal(0,0.5)"))
+iter <- 5000
+options(mc.cores = 2)
+control <- list(adapt_delta = 0.95)
 symp_type <- unique(sdata$sympType)
 conditions <- data.frame(study = unique(sdata$study))
 rownames(conditions) <- unique(sdata$study)
@@ -11,7 +14,8 @@ theme_set(theme_bw())
 ## SMD
 fit_SMD_post <- brm(SMD_post ~ 0 + sympType + (0+sympType|study), 
                     data = sdata, autocor = cor_fixed(V_SMD_post), 
-                    prior = prior, sample_prior = TRUE)
+                    prior = prior, sample_prior = TRUE, iter = iter,
+                    control = control)
 fit_SMD_post
 plot(fit_SMD_post, ask = FALSE)
 (hyp_SMD_post <- hypothesis(fit_SMD_post, paste("sympType", symp_type, " = 0")))
@@ -23,7 +27,7 @@ plot(marginal_effects(fit_SMD_post, conditions = conditions,
 ## SMCR
 fit_SMCR <- brm(SMCR ~ 0 + sympType + (0+sympType|study), data = sdata,
                 autocor = cor_fixed(V_SMCR), prior = prior,
-                sample_prior = TRUE)
+                sample_prior = TRUE, iter = iter, control = control)
 fit_SMCR
 plot(fit_SMCR, ask = FALSE)
 (hyp_SMCR <- hypothesis(fit_SMCR, paste("sympType", symp_type, " = 0")))
@@ -35,13 +39,15 @@ plot(marginal_effects(fit_SMCR, conditions = conditions,
 
 # ---------------- analysis of overall symptoms ---------------- 
 fit_SMD_post_ove <- brm(SMD_post | se(sqrt(vSMD_post)) ~ 0 + intercept + (1|study), 
-                        data = tdata, prior = prior, sample_prior = TRUE)
+                        data = tdata, prior = prior, sample_prior = TRUE,
+                        iter = iter, control = control)
 fit_SMD_post_ove
 (hyp_SMD_post_ove <- hypothesis(fit_SMD_post_ove, "intercept = 0"))
 plot(hyp_SMD_post_ove)
 
 fit_SMCR_ove <- brm(SMCR | se(sqrt(vSMCR)) ~ 0 + intercept  + (1|study), 
-                    data = tdata, prior = prior, sample_prior = TRUE)
+                    data = tdata, prior = prior, sample_prior = TRUE,
+                    iter = iter, control = control)
 fit_SMCR_ove
 (hyp_SMCR_ove <- hypothesis(fit_SMCR_ove, "intercept = 0"))
 plot(hyp_SMCR_ove)
@@ -51,78 +57,80 @@ plot(hyp_SMCR_ove)
 ## assuming the same effects across symptom types
 ## SMD
 fit_SMD_oxyAge <- brm(SMD_post ~ 0 + sympType + oxyAge + (0+sympType|study), 
-                    data = sdata, autocor = cor_fixed(V_SMD_post), 
-                    prior = prior, sample_prior = TRUE)
+                      data = sdata, autocor = cor_fixed(V_SMD_post), 
+                      prior = prior, sample_prior = TRUE, iter = iter,
+                      control = control)
 fit_SMD_oxyAge
 
 fit_SMD_plaAge <- update(fit_SMD_oxyAge, formula. = ~ . + plaAge - oxyAge,
-                         newdata = sdata)
+                         newdata = sdata, control = control)
 fit_SMD_plaAge
 
 fit_SMD_oxyMale <- update(fit_SMD_oxyAge, formula. = ~ . + oxyMale - oxyAge,
-                         newdata = sdata)
+                          newdata = sdata, control = control)
 fit_SMD_oxyMale
 
 fit_SMD_plaMale <- update(fit_SMD_oxyAge, formula. = ~ . + plaMale - oxyAge,
-                          newdata = sdata)
+                          newdata = sdata, control = control)
 fit_SMD_plaMale
 
 fit_SMD_training <- update(fit_SMD_oxyAge, formula. = ~ . + training - oxyAge,
-                          newdata = sdata)
+                           newdata = sdata, control = control)
 fit_SMD_training
 
 fit_SMD_duration <- update(fit_SMD_oxyAge, formula. = ~ . + duration - oxyAge,
-                           newdata = sdata)
+                           newdata = sdata, control = control)
 fit_SMD_duration
 
 fit_SMD_dailyDose <- update(fit_SMD_oxyAge, formula. = ~ . + dailyDose - oxyAge,
-                           newdata = sdata)
+                            newdata = sdata, control = control)
 fit_SMD_dailyDose
 
 fit_SMD_eachDose <- update(fit_SMD_oxyAge, formula. = ~ . + eachDose - oxyAge,
-                           newdata = sdata)
+                           newdata = sdata, control = control)
 fit_SMD_eachDose
 
 fit_SMD_admin_int <- update(fit_SMD_oxyAge, formula. = ~ . + admin_int - oxyAge,
-                           newdata = sdata)
+                            newdata = sdata, control = control)
 fit_SMD_admin_int
 
 # SMCR
 fit_SMCR_oxyAge <- brm(SMCR ~ 0 + sympType + oxyAge + (0+sympType|study), 
                       data = sdata, autocor = cor_fixed(V_SMCR), 
-                      prior = prior, sample_prior = TRUE)
+                      prior = prior, sample_prior = TRUE, iter = iter,
+                      control = control)
 fit_SMCR_oxyAge
 
 fit_SMCR_plaAge <- update(fit_SMCR_oxyAge, formula. = ~ . + plaAge - oxyAge,
-                         newdata = sdata)
+                          newdata = sdata, control = control)
 fit_SMCR_plaAge
 
 fit_SMCR_oxyMale <- update(fit_SMCR_oxyAge, formula. = ~ . + oxyMale - oxyAge,
-                          newdata = sdata)
+                           newdata = sdata, control = control)
 fit_SMCR_oxyMale
 
 fit_SMCR_plaMale <- update(fit_SMCR_oxyAge, formula. = ~ . + plaMale - oxyAge,
-                          newdata = sdata)
+                           newdata = sdata, control = control)
 fit_SMCR_plaMale
 
 fit_SMCR_training <- update(fit_SMCR_oxyAge, formula. = ~ . + training - oxyAge,
-                           newdata = sdata)
+                            newdata = sdata, control = control)
 fit_SMCR_training
 
 fit_SMCR_duration <- update(fit_SMCR_oxyAge, formula. = ~ . + duration - oxyAge,
-                           newdata = sdata)
+                            newdata = sdata, control = control)
 fit_SMCR_duration
 
 fit_SMCR_dailyDose <- update(fit_SMCR_oxyAge, formula. = ~ . + dailyDose - oxyAge,
-                            newdata = sdata)
+                             newdata = sdata, control = control)
 fit_SMCR_dailyDose
 
 fit_SMCR_eachDose <- update(fit_SMCR_oxyAge, formula. = ~ . + eachDose - oxyAge,
-                           newdata = sdata)
+                            newdata = sdata, control = control)
 fit_SMCR_eachDose
 
 fit_SMCR_admin_int <- update(fit_SMCR_oxyAge, formula. = ~ . + admin_int - oxyAge,
-                            newdata = sdata)
+                             newdata = sdata, control = control)
 fit_SMCR_admin_int
 
 
@@ -140,9 +148,11 @@ for (i in seq_along(study_names)) {
                            out_id = subdata$sympType,
                            v = subdata$vSMCR, R = cor_mat)
   fits_SMD_post[[i]] <- update(fit_SMD_post, newdata = subdata,
-                               autocor = cor_fixed(sub_V_SMD_post))
+                               autocor = cor_fixed(sub_V_SMD_post), 
+                               control = control)
   fits_SMCR[[i]] <- update(fit_SMCR, newdata = subdata,
-                           autocor = cor_fixed(sub_V_SMCR))
+                           autocor = cor_fixed(sub_V_SMCR),
+                           control = control)
 }
 
 # ---------------- publication bias ----------------
@@ -153,7 +163,7 @@ par(mfrow=c(4, 2), mar = c(5, 5, 2, 2) + 0.1)
 # positive
 funnel(rma_SMD_pos <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(sdata, sympType == "positive")), 
-       xlab = "Positive symptoms: Hedges' g", cex = dcex, 
+       xlab = "Positive symptoms: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_pos, estimator = "L0")
 funnel(rma_SMCR_pos <- rma(SMCR ~ 1, vi = vSMCR,
@@ -164,7 +174,7 @@ trimfill(rma_SMCR_pos, estimator = "L0")
 # negative
 funnel(rma_SMD_neg <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(sdata, sympType == "negative")), 
-       xlab = "Negative symptoms: Hedges' g", cex = dcex, 
+       xlab = "Negative symptoms: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_neg, estimator = "L0")
 funnel(rma_SMCR_neg <- rma(SMCR ~ 1, vi = vSMCR,
@@ -175,7 +185,7 @@ trimfill(rma_SMCR_neg, estimator = "L0")
 # general
 funnel(rma_SMD_gen <- rma(SMD_post ~ 1, vi = vSMD_post,
                           data = subset(sdata, sympType == "general")), 
-       xlab = "General symptoms: Hedges' g", cex = dcex, 
+       xlab = "General symptoms: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_gen, estimator = "L0")
 funnel(rma_SMCR_gen <- rma(SMCR ~ 1, vi = vSMCR,
@@ -185,7 +195,7 @@ funnel(rma_SMCR_gen <- rma(SMCR ~ 1, vi = vSMCR,
 trimfill(rma_SMCR_gen, estimator = "L0")
 # overall
 funnel(rma_SMD_ove <- rma(SMD_post ~ 1, vi = vSMD_post, data = tdata), 
-       xlab = "Overall symptoms: Hedges' g", cex = dcex, 
+       xlab = "Overall symptoms: SMD", cex = dcex, 
        cex.axis = dcex, cex.lab = dcex)
 trimfill(rma_SMD_ove, estimator = "L0")
 funnel(rma_SMCR_ove <- rma(SMCR ~ 1, vi = vSMCR, data = tdata), 
